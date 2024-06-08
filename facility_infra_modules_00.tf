6616302c53a62b7052913272b190b59faaa5c2e6
biggiebk <biggiebk@mymadlab.net>
2024-06-08 11:32:14 -0700

    Phase two of Workspace renames

---
+++ facility_infra_modules_00.tf
@@ -0,0 +1,11 @@
+module "facility_infra_module_00" {
+  source  = "app.terraform.io/mymadlab/mml_workspace_builder/tfe"
+  version = "~> 0.1.0"
+
+  name              = "facility_infra_modules_00"
+  description       = "Responsible for creating and managing TFE modules with repos"
+  tfe_org           = var.tfe_org
+  github_org        = var.github_org
+  vcs_provider_name = var.vcs_provider_name
+  tags              = ["facility", "infra"]
+}
