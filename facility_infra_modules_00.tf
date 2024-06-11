4dd101a40c365ea948e6d2e14023dede7c70245a
Bryce Klimoski <biggiebk@mymadlab.net>
2024-06-11 16:11:59 -0700

    Enable protected main branch

--- facility_infra_modules_00.tf
+++ facility_infra_modules_00.tf
@@ -1,6 +1,6 @@
 module "facility_infra_module_00" {
   source  = "app.terraform.io/mymadlab/mml_workspace_builder/tfe"
-  version = "~>0.2.0"
+  version = "~>0.3.0"
 
   name              = "facility_infra_modules_00"
   description       = "Responsible for creating and managing TFE modules with repos"
