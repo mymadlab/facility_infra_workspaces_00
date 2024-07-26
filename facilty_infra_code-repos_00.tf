970943e1667f3e7c196552849de731b3022899a4
biggiebk <biggiebk@mymadlab.net>
2024-07-26 13:14:14 -0700

    Create github code repos workspace

---
+++ facilty_infra_code-repos_00.tf
@@ -0,0 +1,12 @@
+module "facility_code_git-repo_00" {
+  source  = "app.terraform.io/mymadlab/mml_workspace_builder/tfe"
+  version = "~>0.3.0"
+
+  name              = "facility_code_git-repos_00"
+  description       = "Create standalone git repos for code."
+  tfe_org           = var.tfe_org
+  github_org        = var.github_org
+  vcs_provider_name = var.vcs_provider_name
+  tags              = ["facility", "code", "repos"]
+
+}
